d6075901e9618a837957af20c2af68ca3ee3e177
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-11-03 23:21:08 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+//dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+//Scraping tools
+var request = require('request');
+var cheerio = require('cheerio');
+
+//use morgan and bodyparser with app
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+	extended:false
+}));
+
+
+// set up handlebars default layout and view engine
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
