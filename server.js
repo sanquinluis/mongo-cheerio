200c17eef55bafd4235b6636ff3002f1f28b073e
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-11-08 21:26:27 -0500

    css

--- server.js
+++ server.js
@@ -129,6 +129,41 @@ app.delete('delete', function(req, res){
 	});
 });
 
+	//replace the existing note of an article  with a new one
+	// or if no note exists for an article, make the posted note it's note.
+
+	app.post('/save', function(req, res){
+		//create a new note and pass the req.body to the entry.
+
+		var newNote = new Note(req.body);
+
+		//and save the new note the db
+
+		newNote.save(function(err, doc){
+			//log any errors
+			if(err){
+				console.log(err);
+			} else {
+				//using the the Article id passed in the id parameter of our url, 
+			// prepare a query that finds the matching Article in our db
+			// and update it to make it's lone note the one we just saved.
+
+			Article.findOneAndUpdate({'_id': req.params.id},{'note': doc._id})
+			//execute the above query
+			.exec(function(err, doc2){
+				//log errors
+				if(err){
+					console.log(err);
+				} else {
+					//or send the document to browser
+					res.send(doc);
+				}
+				
+			});
+		}
+	});
+});
+
 var PORT = process.env.PORT || 3000;
 // listen on port 3000
 app.listen(PORT, function() {
